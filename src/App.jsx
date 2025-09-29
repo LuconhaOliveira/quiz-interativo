80d7c04c13050e9396ff3cac451330e6106cc964
LuconhaOliveira <lucaas.oliveira0254@gmail.com>
2025-09-29 11:13:34 -0300

    ADD: scoreboard

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,31 @@ import styles from './App.module.css'
 import Header from './components/Header';
 import questions from "./data/questions";
 import QuestionCard from "./components/QuestionCard";
+import ScoreBoard from "./components/ScoreBoard";
 
 function App() {
   const [questionIndex,setQuestionIndex] = useState(0);
-  const [timer, setTimer] = useState(0);
   const [answers, setAnswer] = useState([]);
+  const [timer, setTimer] = useState(0);
 
   useEffect(() => {
-    setInterval(() => {
-      setTimer(timer + 1);
-    }, 1000);
+    const intervalId = setInterval(() => {
+    setTimer(prevTimer => prevTimer + 1);
+  }, 1000);
+
+  return () => clearInterval(intervalId);
   });
 
   const answerQuestion = (chose)=>{
-    setAnswer([...answers, { id: questions[questionIndex].id, optionChosen: chose, timer:timer }]);
+    setAnswer([...answers, { id: questions[questionIndex].id, optionChose: chose, time: timer }]);
     setTimer(0);
     setQuestionIndex(questionIndex+1);
+  }
+
+  const resetQuiz = ()=>{
+    setAnswer([]);
+    setTimer(0);
+    setQuestionIndex(0);
   }
 
   useEffect(() => {
@@ -28,8 +37,10 @@ function App() {
 
   return (
     <>
-      <Header currentQuestion={questionIndex+1} totalQuestions={questions.length} timer={timer} />
-      <QuestionCard question={questions[questionIndex].question} options={questions[questionIndex].options} answerQuestion={answerQuestion} />
+      {questionIndex<questions.length?
+      <><Header currentQuestion={questionIndex+1} totalQuestions={questions.length} timer={timer} />
+      <QuestionCard question={questions[questionIndex].question} options={questions[questionIndex].options} answerQuestion={answerQuestion} /></>
+      : <ScoreBoard questions={questions} answers={answers} resetQuiz={resetQuiz} /> }
     </>
   )
 }
---
+++ src/components/ScoreBoard/index.jsx
@@ -0,0 +1,41 @@
+import styles from "./ScoreBoard.module.css"
+
+const ScoreBoard = ({ questions, answers, resetQuiz }) => {
+
+    let pontuacao=0,acertos = 0,menorTempo;
+
+    questions.map((question,i) => {
+        if(question.answer==answers[i].optionChose){
+           acertos+=1;
+           pontuacao+=question.points
+        }
+        if(i==0)menorTempo=i;
+        else if(answers.time<menorTempo) menorTempo=answers.time;
+    });
+
+    return (
+        <section className={styles.results}>
+            <header>
+                <h2>Resultado final</h2>
+                <span>Pontuação final: {pontuacao}</span>
+                <span>Menor tempo: {menorTempo}</span>
+                <span>Acertos: {acertos}</span>
+                <button onClick={resetQuiz}>Reiniciar quiz</button>
+            </header>
+            <div className={styles.cardsList}>
+                {questions.map((question,i) => (
+                    <div key={question.id} className={styles.cardsItem}>
+                        <p>Pergunta: {question.question}</p>
+                        <p>Sua Resposta: {answers[i].optionChose}</p>
+                        <p>Resposta Correta: {question.answer}</p>
+                        <p>Pontos: {question.answer==answers[i].optionChose?question.points:0}</p>
+                        <p>Tempo: {answers[i].time} segundos</p>
+                        <p>{question.answer==answers[i].optionChose?"Correta":"Errada"}</p>
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
+};
+
+export default ScoreBoard
